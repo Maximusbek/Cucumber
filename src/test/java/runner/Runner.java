0ea4acd2e31297ee4628b8e21f6a7096adc5bc4c
Maximusbek <65311736+Maximusbek@users.noreply.github.com>
2020-09-21 11:40:48 -0500

    Update Runner.java

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -6,6 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {"pretty","html:target/cucumber-html-report","json:target/cucumber.json","junit:target/cucumber.xml","rerun:target/rerun.txt"},
         features = "src/test/resources/com.espocrm/espocrm.feature",
         glue = "steps",
         dryRun = false
